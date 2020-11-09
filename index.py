6d167da194d42f8fa9d3e4be2830e0181d18615a
soupoo7 <71666520+soupoo7@users.noreply.github.com>
2020-11-09 14:32:49 +0530

    Update index.py

--- index.py
+++ index.py
@@ -35,7 +35,8 @@ def Task():
 	Press 4: To Configure Web_server
 	Press 5: To configure SSH_Server
 	Press 6: To Start Docker 
-	Press 7: To Exit
+	Press 7: To Create LVM Partition
+	Press 8: To Exit
 	""")
 	
 #Function Asking the Available Options from user to perform that task
@@ -68,8 +69,37 @@ def Options_local(ch):
 
 		elif int(ch)==6:
     			os.system("systemctl start docker")
-
+			
 		elif int(ch)==7:
+			#LVM Creation Automation
+			os.system("tput setaf 3")
+			print("\t\t\tWelcome To LVM Creation")
+			os.system("tput setaf 7")
+			print("\t\t\t------------------------")
+			
+			a1 = input("Enter Disk1 Name :")
+			a2 = input("Enter Disk2 Name :")
+			v = input("Enter Name Of Your Volume Group :")
+			l = input("Enter Name Of Your Logical Volume :")
+			s = input("Enter Size Of Your Logical Volume :")
+			mp = input("Enter Mount Point Of Your LVM :")
+			#pv creation 
+			os.system("pvcreate /dev/"+a1)
+			os.system("pvcreate /dev/"+a2)
+			#vg creation
+			os.system("vgcreate "+v+" /dev/"+a1+" /dev/"+a2)
+			#lv creation
+			os.system("lvcreate --size "+s+" --name "+l+" "+v)
+			#Formatting LVM
+			os.system("mkfs.ext4 /dev/"+v+"/"+l)
+			#mounting LVM
+			os.system("mkdir /"+mp)
+			os.system("mount /dev/"+v+"/"+n+" "+"/"+mp)
+			os.system("cd /"+mp)
+			exit()
+
+
+		elif int(ch)==8:
 			Credits()
 			exit()
 
